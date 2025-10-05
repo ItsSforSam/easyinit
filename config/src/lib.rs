756a214bbaca487d93a6d7655b83f61053309ea5
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-10-05 14:08:25 -0500

    Changed `kern_cmdline` to `config`
    This is due to unify all code about configuration into a single crate

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,16 +1,26 @@
-//! Parses the command line that the kernel was booted with and extracts
-//! easyinit specific options.
+//! Parses the kernel's command line and 
 // cSpell:words Cmdline
 use std::fs::File;
 use std::io::prelude::*;
+use log::LevelFilter;
+
+
 /// Represents parameters passed to easyinit via the kernel command line.
+#[derive(Debug)]
+#[non_exhaustive]
 pub struct Cmdline{
     /// This uses the kernels `quite` and `easyinit.loglevel` options.
     /// 
     /// `quite` is an alias for `easyinit.loglevel=0`.
     /// 
     /// `easyinit.loglevel` will take priority if both are present.
-    loglevel: bool,
+    /// 
+    /// Default is Warning level (3)
+    pub loglevel: LevelFilter,
+
+    
+
+    
 }
 impl Cmdline{
     /// Reads the command line from `/proc/cmdline` and parses it.
@@ -25,12 +35,17 @@ impl Cmdline{
     /// 
     /// Used for testing.
     pub fn use_file(f:File)->Self{
-        todo!()
+        let mut r = Cmdline::default();
+        
+
+        r
     }
 }
 impl Default for Cmdline{
     /// Returns the default options for `Cmdline`.
     fn default()->Self{
-        Self::new()
+        Cmdline {
+            loglevel: LevelFilter::Warn
+        }
     }
 }
