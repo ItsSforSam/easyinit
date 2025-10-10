a7880649db60ef66d1f8696ab3006a095cf46f60
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-10-10 14:14:41 -0500

    Signaling logic

--- src/signals.rs
+++ src/signals.rs
@@ -1,11 +1,20 @@
 use std::sync::atomic::AtomicBool;
-
+use nix::sys::signal::{sigaction,SaFlags,Signal,SigAction,SigHandler,SigSet};
 pub fn setup_signals(){
-    use signal_hook::consts::{TERM_SIGNALS,signal as sigs};
+    
     
     let sig_running = std::sync::Arc::new(AtomicBool::new(false));
-    
-    for sig in TERM_SIGNALS{
-        unsafe { libc::signal(*sig,libc::SIG_IGN)};
+    sig_ignore(Signal::SIGUSR1).unwrap();
+    sig_ignore(Signal::SIGUSR2).unwrap();
+}
+
+
+fn sig_ignore(signal: Signal)->nix::Result<SigAction>{
+    // SAFETY: The function just ignores the signal
+    unsafe {
+        sigaction(signal, &SigAction::new(
+            SigHandler::SigIgn,
+             SaFlags::SA_NOCLDWAIT ,
+             SigSet::empty()))
     }
 }
