f892287c3b1808f065ff5fb5294cce4108b8636c
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-10-14 11:51:12 -0500

    Fixed main.rs with using utils crate

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 pub mod util;
-mod signals;
+
 
 fn main() -> ! {
     // signal_hook::flag::register(signal, flag);
 
     // SAFETY: In the current execution, we can safely say that there are no other threads reading or writing to the environment
-    unsafe {
-        // These environment 
-        std::env::remove_var("RUST_LIB_BACKTRACE");
-        std::env::remove_var("RUST_BACKTRACE");
-    }
+    unsafe {::utils::correct_env();}
     // SAFETY: No logging implementation is called previously
     unsafe { logging::init().unwrap_unchecked() }
 
