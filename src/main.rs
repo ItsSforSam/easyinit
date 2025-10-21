a8df772fbb9586941aa6a8c3836a95fc93729ea6
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-10-21 12:00:01 -0500

    Added to use custom panic handler in main executable (the init system)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() -> ! {
 
     // SAFETY: In the current execution, we can safely say that there are no other threads reading or writing to the environment
     unsafe {::utils::correct_env();}
+    panic_handler::switch_panic();
     // SAFETY: No logging implementation is called previously
     unsafe { logging::init().unwrap_unchecked() }
 
