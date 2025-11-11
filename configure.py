e5584e444523a855d9bf29ef8d7e23019ae18b62
SforSam <81501696+ItsSforSam@users.noreply.github.com>
2025-11-11 14:32:43 -0600

    Updating configure script

--- configure.py
+++ configure.py
@@ -7,19 +7,50 @@ from pathlib import Path
 import sys
 import tomllib
 import argparse
+import importlib
+logging = importlib.import_module("tools.py-logging") # allows importing from tools/py-logging
 
 ROOT = Path(os.path.dirname(__file__))
 __version__ = "0.0.1"
 
+BUILDABLES = [ "easyinit", "easyctl", "journald"]
+"""
+Packages that are provided
+"""
+
+UNSTABLE_FEATURES = ["selinux","systemd"]
+"""
+Provides a warning that unstable features are being used.
+"""
+
+l = logging.get_logger("configure")
+
 def main():
     parsed, conf =  get_parser().parse_known_args()
+    conf = parse_conf(conf)
+    if parsed.verbose:
+        l.setLevel(logging.l.DEBUG)
+    generate_makefile(conf, parsed)
+    
 
 def get_version() -> str:
     with open(ROOT / "Cargo.toml", "rb") as f:
         data = tomllib.load(f)
     return data["workspace"]["package"]["version"]
 
+# def setup_logger():
+#     import logging
+#     logger = logging.getLogger("configure")
+#     handler = logging.StreamHandler()
+#     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+#     handler.setFormatter(formatter)
+#     logger.addHandler(handler)
+#     logger.setLevel(logging.INFO)
+#     return logger
 
+def eprint(*args, **kwargs):
+    file = kwargs.pop("file", sys.stderr)
+    print(*args, file=sys.stderr, **kwargs)
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description="Configure EasyInit before building."
@@ -55,13 +86,13 @@ def get_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         "--output", "-o",
         help="Where to put the generated Makefile. Default is in the root of the repo",
-        default=ROOT / "Makefile.config",
+        default=ROOT / "Makefile",
         type=Path,
         metavar="PATH"
     )
     parser.add_argument(
     "--systemd",
-    help="Allow systemd integration. Default is when systemd is detected on the system. ()",
+    help="Allow systemd integration. Default is when systemd is detected on the system. (Detected: %(default)s)",
     default=is_systemd(),
     action=OptionalFeature,
     )
@@ -73,7 +104,8 @@ def is_systemd() -> bool:
     Check if systemd is available on this system
     
     If systemctl is in PATH, we assume systemd is available.
-    This is to prevent leaving a system stranded without proper service files.
+    This is to prevent leaving a system stranded without proper service files and to make an 
+    easier transition.
 
     """
     import shutil
@@ -128,10 +160,14 @@ class OptionalFeature(argparse.Action):
     def __call__(self, parser, namespace, values, option_string = None):
         if option_string in self.option_strings:
             if option_string.startswith("--enable-") or option_string.startswith("--with-"):
+                l.warn(f"Enabling unstable {self.feature_name} feature")
                 setattr(namespace, self.dest, True)
             elif option_string.startswith("--disable-") or option_string.startswith("--without-"):
                 setattr(namespace, self.dest, False)
-            # Neither, use default
+            elif callable(self.default): # allows lazy evaluation of defaults
+                setattr(namespace, self.dest, self.default())
+            else:
+                setattr(namespace, self.dest, self.default)
     
     def format_usage(self) -> str:
         
@@ -165,16 +201,62 @@ def parse_conf(conf:list[str])->Conf:
         options[a[0].lower()] = a[1]
     return Conf(**options)
 
-
+def gen_flags(parsed:argparse.Namespace) -> str:
+    flags = ["--release","--no-default-features",]
+    features = []
+    if parsed.selinux:
+        features.append("selinux")
+    if parsed.coreutils:
+        features.append("coreutils")
+    if parsed.systemd:
+        features.append("systemd")
+    if len(features) != 0:
+        f= ",".join(features)
+        flags.append(f"--features={f}")
+    return " ".join(flags)
+def rtarget() -> str:
+    global BUILDABLES
+    b = []
+    for build in BUILDABLES:
+        b.append(f"$(RTARGET)/{build}")
+    return " ".join(b)
+def gen_buildables(parsed:argparse.Namespace) -> str:
+    global BUILDABLES
+    i = []
+    for b in BUILDABLES:
+        """
+$RTARGET/{b}:
+        """
+    return "\n".join(i)
+def gen_install(parsed:argparse.Namespace) -> str:
+    global BUILDABLES
+    i = []
+    for b in BUILDABLES:
+        """
+install-{b}: {b}
+\t
+        """
+    return "\n".join(i)
 def generate_makefile(conf:Conf, parsed:argparse.Namespace) -> None:
     f= f"""
-    # A generated Makefile for Easyinit. Don't edit this file directly!
-    # any issues with this file should be investigated in configure.py
-    RUSTC = {conf.rustc}
-    CARGO = {conf.cargo}
-    PREFIX = {parsed.prefix}
-    # FLAGS := 
-    """
+# A generated Makefile for Easyinit. Don't edit this file directly!
+# any issues with this file should be investigated in configure.py
+RUSTC = {conf.rustc}
+CARGO = {conf.cargo}
+PREFIX = {parsed.prefix}
+FLAGS := {gen_flags(parsed)}
+RTARGET := target/release/
+all: {rtarget()}
+\t $(CARGO) build $(FLAGS)
+
+install: all
+
+.PHONY: install
+"""
+    l.info(f"Writing Makefile to {parsed.output}")
+    l.info(f"Makefile contents:\n{f}")
+    with open(parsed.output, "w") as mf:
+        mf.write(f)
 
 if __name__ == "__main__":
     main()
